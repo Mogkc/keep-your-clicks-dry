8b0378e4b840aa750857ea6e4031455e3ab221e7
Mogkc <carson.mogk@cox.net>
2019-04-22 15:10:57 -0700

    able to randomize images

--- src/App.js
+++ src/App.js
@@ -5,29 +5,56 @@ import './App.css';
 
 class App extends Component {
   state = {
-    images: [{ src: "http.cat/404", alt: "not found", clicked: false }],
+    images: [
+      { src: "https://http.cat/404", alt: "Not Found", clicked: false },
+      { src: "https://http.cat/200", alt: "OK", clicked: false },
+      { src: "https://http.cat/500", alt: "Server Error", clicked: false },
+      { src: "https://http.cat/300", alt: "Multiple Choices", clicked: false }
+    ],
     score: 0,
     highScore: 0
   }
 
+  componentDidMount() {
+    this.randomizeImages();
+  }
+
   randomizeImages = () => {
-    // To be implemented after images component is fleshed out
+    // Variables we need to randomize the array
+    const randomized = [];
+    // Slots is the number of empty spots left in the array
+    let slots = this.state.images.length;
+    this.state.images.forEach(img => {
+      // Place is the number of empty spots that are passed over
+      let index = 0, place = Math.floor(Math.random() * slots);
+
+      while (place > 0 || randomized[index]) {
+        // Only decreace place when passing over an availible spot in randomized
+        if (!randomized[index]) place--;
+        // Always increace the index where the image will be pushed
+        index++;
+      }
+      randomized[index] = img;
+      slots--;
+    });
+
+
+    this.setState({ images: randomized });
   }
 
   firstClick = (src) => {
-    this.randomizeImages();
     this.setState({
       score: this.state.score + 1,
       images: this.state.images.map(img => {
-        (img.src === src) ? img.clicked = true : img.clicked = false;
+        if (img.src === src) img.clicked = true;
         return img;
       })
     });
+    this.randomizeImages();
   }
 
   handleLoss = () => {
     // Reset state.images, then
-    this.randomizeImages();
     return (this.state.score > this.state.highScore) ?
       this.setState({ highScore: this.state.score, score: 0 }) :
       this.setState({ score: 0 });
@@ -38,7 +65,7 @@ class App extends Component {
       <div>
         <ScoreBar score={this.state.score} highScore={this.state.highScore} />
         {this.state.images.map(img =>
-          <Image image={img} clickFunction={(img.clicked) ? this.handleLoss : this.firstClick} />
+          <Image image={img} clickFunction={img.clicked ? this.handleLoss : this.firstClick} />
         )}
       </div>
     );
