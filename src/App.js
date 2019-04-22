2a5395f5e976bf4fd6c18b792d82c2f2444c49b5
Mogkc <carson.mogk@cox.net>
2019-04-22 11:54:16 -0700

    added functions for when an image is clicked

--- src/App.js
+++ src/App.js
@@ -10,6 +10,22 @@ class App extends Component {
     highScore: 0
   }
 
+  randomizeImages = () => {
+    // To be implemented after images component is fleshed out
+  }
+
+  firstClick = () => {
+    randomizeImages();
+    this.setState({ score = this.state.score + 1 });
+  }
+
+  handleLoss = () => {
+    randomizeImages();
+    return (this.state.score > this.state.highScore) ?
+      setState({ highScore: this.state.score, score: 0 }) :
+      setState({ score: 0 });
+  }
+
   render() {
     return (
       <ScoreBar score={this.state.score} highScore={this.state.highScore} />
